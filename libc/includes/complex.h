b5d703cee27e251aa06fdb62e55c9049e97e2139
Dwight Guth <dwightguth@runtimeverification.com>
2014-12-02 18:33:20 -0600

    minor fixes to c semantics

--- libc/includes/complex.h
+++ libc/includes/complex.h
@@ -2,6 +2,6 @@
 #define _KCC_COMPLEX_H
 #include <kcc_settings.h>
 
-#error complex.h not supported
+#define __STDC_NO_COMPLEX__
 
 #endif
--- libc/includes/errno.h
+++ libc/includes/errno.h
@@ -2,6 +2,12 @@
 #define _KCC_ERRNO_H
 #include <kcc_settings.h>
 
-#error errno.h not supported
+#define _ERRNO_H 1
+#define __ASSEMBLER__
+#include <bits/errno.h> /* get system-specific errno constants */
+#undef __ASSEMBLER
+#undef errno
+extern int *__kcc_errno();
+#define errno *__kcc_errno()
 
 #endif
